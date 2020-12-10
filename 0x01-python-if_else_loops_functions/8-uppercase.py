1b510f54d137827b1b826f7be67e16ce35155957
andresgfranco <andresgfranco0@gmail.com>
2020-12-10 00:48:24 -0500

    Correction on pep8 style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-           for i in str:
-                if ord(i) >= 97 and ord(i) <= 122:
-                    x = ord(i) - 32
-                else:
-                    x = ord(i)
-                print("{}".format(chr(x)), end="")
-            print()
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            x = ord(i) - 32
+        else:
+            x = ord(i)
+        print("{}".format(chr(x)), end="")
+    print()
