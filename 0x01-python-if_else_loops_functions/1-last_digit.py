d11e6a52b7adc1d47f60bde7f5fc76504e37161f
andresgfranco <andresgfranco0@gmail.com>
2020-12-09 22:10:02 -0500

    Adding script number 1 with pep8 style

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+if number < 0:
+    last_digit = (number * -1) % 10
+print("Last digit of {} is {} and is ".format(number, last_digit), end="")
+if last_digit > 5:
+    print("greater than 5")
+elif last_digit < 6 and last_digit != 0:
+    print("less than 6 and not 0")
+else:
+    print("0")
