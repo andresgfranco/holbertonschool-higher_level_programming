9f87d82d34c8869c37c6db6745236b9c16414c08
andresgfranco <andresgfranco0@gmail.com>
2020-12-09 23:54:34 -0500

    Correction on mispelling

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for number1 in range(0, 10):
     for number2 in range(0, 10):
         if number2 > number1:
             if number1 == 8 and number2 == 9:
-                print("{}{}". format(number1, number2))
+                print("{}{}".format(number1, number2))
             else:
-                print("{}{}, ". format(number1, number2), end="")
+                print("{}{}, ".format(number1, number2), end="")
