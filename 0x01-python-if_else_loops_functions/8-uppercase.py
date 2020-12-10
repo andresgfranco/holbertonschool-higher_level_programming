de85909b6da33351556ca4d23e91e15d002970d8
andresgfranco <andresgfranco0@gmail.com>
2020-12-10 00:42:47 -0500

    Correction on style and missing conditional

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-        for i in str:
+           for i in str:
                 if ord(i) >= 97 and ord(i) <= 122:
                     x = ord(i) - 32
-                    print("{}".format(chr(x)))
                 else:
-                    print(i)
+                    x = ord(i)
+                print("{}".format(chr(x)), end="")
+            print()
