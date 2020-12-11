d976b1877a9da52dbec537736d16b5e436609027
andresgfranco <andresgfranco0@gmail.com>
2020-12-10 21:56:23 -0500

    Correction on a character

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if i == 1:
         print("0 arguments.")
     else:
-        print("{} argument{}.".format(i - 1, "s" if i > 2 else ""))
+        print("{} argument{}:".format(i - 1, "s" if i > 2 else ""))
         for counter in range(1, i):
             print("{}: {}".format(counter, argv[counter]))
