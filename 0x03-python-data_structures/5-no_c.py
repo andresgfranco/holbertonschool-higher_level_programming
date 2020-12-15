5e4d2394992b524c2739dcd409c3222adc8ddbc1
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 16:23:40 -0500

    Simplified code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    c = 0
+    new_string = ""
     for i in my_string:
         if i == 'c' or i == 'C':
-            my_string = my_string[:c] + my_string[c + 1:]
             continue
-        c += 1
-    return my_string
+        new_string += new_string + i
+    return new_string
