39040cd83d38ca2ba4f0bee9120412507a044a98
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 16:25:02 -0500

    Correction on statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     for i in my_string:
         if i == 'c' or i == 'C':
             continue
-        new_string += new_string + i
+        new_string = new_string + i
     return new_string
