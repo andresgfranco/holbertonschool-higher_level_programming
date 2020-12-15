c9c7e4c27a5092a04e7796ea771be4e40a1df34a
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 18:09:50 -0500

    Correction on conditionals within last for

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
             for i in matrix:
                 for count, j in enumerate(i):
-                    print("{:d}".format(j), end='')
                     if count != (len(matrix) - 1):
-                        print("{:s}".format(" "), end='')
-                print("")
+                        print("{:d} ".format(j), end='')
+                    else:
+                        print("{:d}".format(j))
     else:
         print("")
