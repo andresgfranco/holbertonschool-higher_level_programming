c739752cffcebb3eed33fba88515dff3687082ee
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 18:07:41 -0500

    Added a format to print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
                 for count, j in enumerate(i):
                     print("{:d}".format(j), end='')
                     if count != (len(matrix) - 1):
-                        print("{}".format(" "), end='')
+                        print("{:s}".format(" "), end='')
                 print("")
     else:
         print("")
