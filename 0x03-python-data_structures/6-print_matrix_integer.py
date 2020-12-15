56b12b790cb5d5ba88160cb7e7663fa7403b65a5
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 17:59:17 -0500

    Missing sintaxis in first for

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i matrix:
+    for i in matrix:
         for count, j in enumerate(i):
             print("{:d}".format(j), end='')
             if count != (len(matrix) - 1):
