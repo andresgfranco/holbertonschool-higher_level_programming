a4468e3cc7209bc1297b82b615973be01cfe665f
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 18:19:44 -0500

    Corrected conditional when matrix doesn't exist

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if matrix != [[]]:
             for i in matrix:
                 for count, j in enumerate(i):
-                    if count != (len(matrix) - 1):
-                        print("{:d} ".format(j), end='')
-                    else:
+                    if count == (len(matrix) - 1):
                         print("{:d}".format(j))
+                    else:
+                        print("{:d} ".format(j), end='')
     else:
-        print("")
+        print('')
