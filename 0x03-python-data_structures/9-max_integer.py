bb162675f293ed7025f37a8f3f85f35f905aab58
andresgfranco <andresgfranco0@gmail.com>
2020-12-15 21:21:59 -0500

    Correction on statements when all numbers are negative

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list != []:
-        max = 0
+        max = my_list[0]
         for i in my_list:
-            if i > max:
+            if i >= max:
                 max = i
         return max
     else:
