d2be8c7066c64fdb81629aea5e38b4d7b8313529
andresgfranco <andresgfranco0@gmail.com>
2020-12-17 17:51:57 -0500

    Correction on pep8 style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[[]]):
     new_matrix = list(map(list, matrix))
     for count_1, i in enumerate(matrix):
         for count_2, j in enumerate(i):
-            new_matrix[count_1][count_2] = j * j     
+            new_matrix[count_1][count_2] = j * j
     return(new_matrix)
