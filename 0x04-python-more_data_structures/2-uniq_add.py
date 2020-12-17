5f6e0b65ce761fb2564342e7c78572638792d36a
andresgfranco <andresgfranco0@gmail.com>
2020-12-17 18:20:26 -0500

    Adding function number 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_list = []
+    for i in my_list:
+        if i not in unique_list:
+            unique_list.append(i)
+    sum_ulist = sum(unique_list)
+    return sum_ulist
