66a5972f5e37467401c4549a36bbb17e39c60db9
andresgfranco <andresgfranco0@gmail.com>
2021-01-04 21:12:19 -0500

    Adding function number 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_elements = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end='')
+            printed_elements += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    print()
+    return (printed_elements)
