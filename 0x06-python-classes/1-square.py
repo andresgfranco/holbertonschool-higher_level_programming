5867ddbdc7de03a1c4593064c6ab28a1df322146
andresgfranco <andresgfranco0@gmail.com>
2021-01-05 22:13:23 -0500

    Adding function number 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a private instance attribute"""
+
+
+class Square:
+    """Define a private instance attribute: size"""
+    def __init__(self, size):
+        self.__size = size
