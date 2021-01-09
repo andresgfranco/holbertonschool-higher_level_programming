9def05ebc379baf27bcec57ce8897b31e337591f
andresgfranco <andresgfranco0@gmail.com>
2021-01-09 19:32:43 +0000

    Adding function number 5 and corresponding test file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Module for text_indentation function
+'''
+
+
+def text_indentation(text):
+    '''Function that a text with 2 new lines
+    after each of these characters: .,? and :
+    '''
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] in ('.', '?', ':'):
+            print("{}\n".format(text[i]))
+            i += 1
+            if i + 1 < len(text):
+                while text[i + 1] == " ":
+                    i += 1
+        else:
+            print(text[i], end='')
+        i += 1
