37fc62ee2c17857174650276d44ed1e3efaa4c73
andresgfranco <andresgfranco0@gmail.com>
2021-01-09 16:30:49 +0000

    Corrected syntax on print

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module for say_my_name function
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''Function that prints
+    My name is <first name> <last name>
+    '''
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
