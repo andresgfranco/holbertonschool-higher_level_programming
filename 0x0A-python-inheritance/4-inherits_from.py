4076f23a56ddc7aff49f42411ecac3ffcf38b527
andresgfranco <andresgfranco0@gmail.com>
2021-01-19 23:38:13 +0000

    Correction on pep8 style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    '''returns True if the object is an instance of a class 
-    that inherited (directly or indirectly) 
+    '''returns True if the object is an instance of a class
+    that inherited (directly or indirectly)
     from the specified class ; otherwise False.'''
     return issubclass(type(obj), a_class) and type(obj) != a_class
