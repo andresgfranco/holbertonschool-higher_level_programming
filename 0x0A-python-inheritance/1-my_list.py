2387ea56d577419eb590db3c7603906617ce2d38
andresgfranco <andresgfranco0@gmail.com>
2021-01-19 23:22:28 +0000

    Adding function number 1, 2, and 3 with the test file for 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module to create a class called MyList'''
+
+
+class MyList(list):
+    '''Class that inherits from list'''
+    def print_sorted(self):
+        '''Method to print a list of sorted integers'''
+        return sorted(self)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for the function is_same_class'''
+
+
+def is_same_class(obj, a_class):
+    ''' returns True if the object is exactly an instance
+    of the specified class ; otherwise False.'''
+    return isinstance(obj, a_class)
