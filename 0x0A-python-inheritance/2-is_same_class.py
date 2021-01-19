f0a1fc0f282110be29e183a8d8726546a219e60e
andresgfranco <andresgfranco0@gmail.com>
2021-01-19 23:35:28 +0000

    Correction on conditional for function 2 return and style on function 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     ''' returns True if the object is exactly an instance
     of the specified class ; otherwise False.'''
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
