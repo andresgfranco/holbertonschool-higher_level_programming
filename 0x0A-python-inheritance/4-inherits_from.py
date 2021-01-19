b26560a7610ca8e52575d6c799f412907ec6f29b
andresgfranco <andresgfranco0@gmail.com>
2021-01-19 23:37:11 +0000

    Adding function number 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usi/bin/python3
+'''Module for the function inherits_from'''
+
+
+def inherits_from(obj, a_class):
+    '''returns True if the object is an instance of a class 
+    that inherited (directly or indirectly) 
+    from the specified class ; otherwise False.'''
+    return issubclass(type(obj), a_class) and type(obj) != a_class
