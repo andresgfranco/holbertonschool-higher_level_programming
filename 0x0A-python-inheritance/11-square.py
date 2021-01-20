a8996671b771defb4067f97d6cf87aa5770ed225
andresgfranco <andresgfranco0@gmail.com>
2021-01-20 02:23:03 +0000

    Adding function number 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module for class Square creation'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Class Square that inherits
+    from Rectangle'''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        '''Method that returns to a description about this class'''
+        return ("[Square] {}/{}".format(self.__size, self.__size))
