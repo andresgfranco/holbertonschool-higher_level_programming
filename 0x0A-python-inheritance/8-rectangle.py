fabd03e72d6cdd83d2e63d1497337f41ce63e231
andresgfranco <andresgfranco0@gmail.com>
2021-01-20 00:25:28 +0000

    Adding function number 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Module for Rectangle class'''
+
+
+class BaseGeometry():
+    '''class BaseGeometry'''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''Class Rectangle that
+    inherits from BaseGeometry'''
+    def __init__(self, width, height):
+        self.integer_validator("heigth", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
