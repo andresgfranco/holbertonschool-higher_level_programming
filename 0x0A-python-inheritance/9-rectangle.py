6d935cd1c6be1125ef072704963988043afecbb1
andresgfranco <andresgfranco0@gmail.com>
2021-01-20 00:46:14 +0000

    Adding comments

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -27,6 +27,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        '''Function to calculate area'''
         return self.__width * self.__height
 
     def __str__(self):
