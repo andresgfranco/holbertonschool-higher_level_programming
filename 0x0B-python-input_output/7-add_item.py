70c69bb47c906b73ab8405affaa157e97cbdecc9
andresgfranco <andresgfranco0@gmail.com>
2021-01-21 03:07:49 +0000

    Correction on pep8 style

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Script hat adds all arguments 
+'''Script hat adds all arguments
 to a Python list, and then save them to a file'''
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
