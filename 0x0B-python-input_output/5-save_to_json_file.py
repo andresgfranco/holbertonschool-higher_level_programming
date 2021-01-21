da781cee31d925d8ef8696870e98ee109f452d60
andresgfranco <andresgfranco0@gmail.com>
2021-01-21 03:12:07 +0000

    Correction on return

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''Method from json to file'''
     with open(filename, 'w') as filename:
-        json.dump(my_obj, filename, indent=4)
+        json.dump(my_obj, filename)
