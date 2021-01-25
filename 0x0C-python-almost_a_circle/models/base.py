61ad179ab632b9a5c056e349d31d029e745e5385
andresgfranco <andresgfranco0@gmail.com>
2021-01-25 02:13:20 +0000

    Updated until task number 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Module for the class Base'''
-
+import json
 
 class Base():
     '''This class will be the "base" of all
@@ -16,3 +16,40 @@ class Base():
         else:
             self.id = Base.__nb_objects + 1
             Base.__nb_objects = self.id
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Returns the JSON string representation of
+        list_dictionaries'''
+        if list_dictionaries == None or len(list_dictionaries) <= 0:
+                return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of the JSON string
+        representation json_string'''
+        if json_string == None or len(json_string) <= 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of
+        list_objs to a file'''
+        if list_objs == None:
+            list_objs = []
+        else:
+            dict_list = []
+            for i in range(0, len(list_objs)):
+                dict_list.append(list_objs[i].to_dictionary())
+            dict_list = cls.to_json_string(dict_list)
+        with open("{}.json".format(cls.__name__), 'w') as file_class:
+            file_class.write(dict_list)
+    @classmethod
+    def create(cls, **dictionary):
+        '''Returns an instance with all attributes
+        already set'''
+        dummy = cls.__name__()
+        dummy.update(dictionary)
+        return dummmy
