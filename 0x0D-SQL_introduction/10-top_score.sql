23ddacc91c625a550ce77926b79c8a3a629a2d32
andresgfranco <andresgfranco0@gmail.com>
2021-03-01 22:17:48 +0000

    Adding script number 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- display both the score and the name (in this order)
+SELECT score, name FROM second_table
