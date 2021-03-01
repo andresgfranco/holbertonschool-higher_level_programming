e239e5f715845f443a58ff68db75081bdd704aec
andresgfranco <andresgfranco0@gmail.com>
2021-03-01 22:21:16 +0000

    Correction on order by score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- display both the score and the name (in this order)
-SELECT score, name FROM second_table
+SELECT score, name
+FROM second_table
+ORDER BY score
