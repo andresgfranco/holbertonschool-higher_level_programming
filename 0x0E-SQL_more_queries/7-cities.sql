c5b6cb49e945fda902bbca6d8ed6fa702754087f
andresgfranco <andresgfranco0@gmail.com>
2021-03-03 00:21:11 +0000

    Missing a comma

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	`id` int AUTO_INCREMENT PRIMARY KEY,
-	`state_id` int NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id)
+	`state_id` int NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
 	`name` VARCHAR(256) NOT NULL,
 );
