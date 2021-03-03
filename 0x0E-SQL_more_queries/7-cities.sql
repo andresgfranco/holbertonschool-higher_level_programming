68c4c02f731605ac755512db1e8a2e8b505c695e
andresgfranco <andresgfranco0@gmail.com>
2021-03-03 00:23:47 +0000

    Removed extra comma

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	`id` int AUTO_INCREMENT PRIMARY KEY,
 	`state_id` int NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
-	`name` VARCHAR(256) NOT NULL,
+	`name` VARCHAR(256) NOT NULL
 );
