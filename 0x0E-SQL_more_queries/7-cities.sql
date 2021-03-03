8a4b6c229fa4899bde41d3f726aed597002d8e7b
andresgfranco <andresgfranco0@gmail.com>
2021-03-03 00:17:52 +0000

    Adding script number 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	`id` int AUTO_INCREMENT PRIMARY KEY,
-	`state_id` int NOT NULL FOREIGN KEY REFERENCES states(id),
-	`name` VARCHAR(256) NOT NULL
+	`state_id` int NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
