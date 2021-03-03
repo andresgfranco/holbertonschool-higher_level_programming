9e6099507a1ca5f8e04909043b9dfeb84778fb87
andresgfranco <andresgfranco0@gmail.com>
2021-03-03 03:21:51 +0000

    Adding missing s

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show
+FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
