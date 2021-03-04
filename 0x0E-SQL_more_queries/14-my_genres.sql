49a447a8638309976169c4c3c0a45be587e5dd8d
andresgfranco <andresgfranco0@gmail.com>
2021-03-04 02:50:46 +0000

    Removed asc

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,4 +9,4 @@ SELECT tv_genres.name
 	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
 	WHERE tv_shows.title = "Dexter"
-	ORDER BY tv_genres.name ASC;
+	ORDER BY tv_genres.name;
