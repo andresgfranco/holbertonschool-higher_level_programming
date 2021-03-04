2ac157ec946c5a8d3a06f31631e5c30e9f677ab7
andresgfranco <andresgfranco0@gmail.com>
2021-03-04 02:28:09 +0000

    Adding missing tasks

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name ASC;
