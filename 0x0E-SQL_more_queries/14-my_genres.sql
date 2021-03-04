90d5929b3f184a5f8b1bcdfa94268bb318d2a28c
andresgfranco <andresgfranco0@gmail.com>
2021-03-04 02:41:17 +0000

    New identation

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,8 +5,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name
-FROM tv_show_genres
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name ASC;
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY tv_genres.name ASC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_genres.name
-FROM tv_show_genres
-RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+	FROM tv_show_genres
+	RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	ORDER BY tv_shows.title, tv_genres.name ASC;
