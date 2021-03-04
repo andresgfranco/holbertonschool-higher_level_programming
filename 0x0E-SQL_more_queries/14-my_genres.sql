f3b328cc7ae42e307817eed485c5db8565a76010
andresgfranco <andresgfranco0@gmail.com>
2021-03-04 02:58:25 +0000

    Correction on misspelled character

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,6 +7,6 @@
 SELECT tv_genres.name
 	FROM tv_show_genres
 	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
+	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 	WHERE tv_shows.title = "Dexter"
-	ORDER BY tv_genres.name;
+	ORDER BY tv_genres.name ASC;
