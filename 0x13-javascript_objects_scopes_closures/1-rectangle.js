aa8fcd3c962ae99ae8d5ef82607051572d77159e
andresgfranco <andresgfranco0@gmail.com>
2021-03-23 18:22:39 -0500

    Correction on conditionals

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (w > 1 || h > 1 ) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
