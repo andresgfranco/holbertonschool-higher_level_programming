d5a8cf69aa7d2e33a10f7b836b5c40b04ae6dd54
andresgfranco <andresgfranco0@gmail.com>
2021-03-23 18:18:03 -0500

    Adding new methods to the rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
