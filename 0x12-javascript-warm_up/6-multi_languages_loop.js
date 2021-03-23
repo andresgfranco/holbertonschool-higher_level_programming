4a8815fb97ab0b31e349b6aa8db540d432015d32
andresgfranco <andresgfranco0@gmail.com>
2021-03-22 20:55:00 -0500

    Adding script number 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; stringArray[i] !== undefined; i++) {
+  console.log(stringArray[i]);
+}
