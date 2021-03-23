ce46a1c9da602f8c6f46bd07ec50c79d1e3877a7
andresgfranco <andresgfranco0@gmail.com>
2021-03-22 19:35:44 -0500

    Adding script number 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 if (!process.argv[2]) {
   console.log('No argument');
+} else {
+  const arg = process.argv[2];
+  console.log(arg);
 }
-const arg = process.argv[2];
-console.log(arg);
