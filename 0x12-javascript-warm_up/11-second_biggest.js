80208fdd42c48390ff34de60512a0db037eb608f
andresgfranco <andresgfranco0@gmail.com>
2021-03-22 23:38:05 -0500

    Correction on typo with length method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,16 @@ const args = process.argv;
 let max = 0;
 let secondMax = 0;
 
-if (!args || args.lenght <= 1) {
+if (!args || args.length <= 1) {
   console.log(0);
-}
-if (args.lenght >= 2) {
-  for (let i = 0; i < args.lenght; i++) {
+} else {
+  for (let i = 0; i < args.length; i++) {
     if (parseInt(args[i]) > max) {
       max = parseInt(args[i]);
     }
   }
 
-  for (let j = 0; j < args.lenght; j++) {
+  for (let j = 0; j < args.length; j++) {
     if (parseInt(args[j]) < max && parseInt(args[j]) > secondMax) {
       secondMax = parseInt(args[j]);
     }
