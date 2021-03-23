5e5d5bdfdafe2c68e0d9d2022c86763fa25becf7
andresgfranco <andresgfranco0@gmail.com>
2021-03-22 23:35:07 -0500

    Hot fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
-const args = process.argv;
 
+const args = process.argv;
 let max = 0;
 let secondMax = 0;
-let i, j;
 
 if (!args || args.lenght <= 1) {
   console.log(0);
-} else {
-  for (i = 0; i < args.lenght; i++) {
+}
+if (args.lenght >= 2) {
+  for (let i = 0; i < args.lenght; i++) {
     if (parseInt(args[i]) > max) {
       max = parseInt(args[i]);
     }
   }
 
-  for (j = 0; j < args.lenght; j++) {
+  for (let j = 0; j < args.lenght; j++) {
     if (parseInt(args[j]) < max && parseInt(args[j]) > secondMax) {
       secondMax = parseInt(args[j]);
     }
