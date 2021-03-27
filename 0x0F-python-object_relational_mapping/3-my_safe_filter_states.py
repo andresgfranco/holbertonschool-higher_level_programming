b45a59f4ddbe26f41eaff40f730947c153614620
andresgfranco <andresgfranco0@gmail.com>
2021-03-27 16:55:44 +0000

    Added documentation and main conditional at the beginning

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,12 +5,16 @@ write one that is safe from MySQL injections!"""
 import MySQLdb
 import sys
 
-def main():
+
+if __name__ == "__main__":
+    """Function that takes in arguments and displays all values in the
+    states table of db where name matches the argument.
+    Safe of MySQL injections"""
     db = MySQLdb.connect(user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            host="localhost")
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host="localhost")
 
     cur = db.cursor()
 
@@ -21,6 +25,3 @@ def main():
 
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
