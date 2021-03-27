24e73859542f14ef5886aca411d3cb6d09ec4665
andresgfranco <andresgfranco0@gmail.com>
2021-03-27 16:35:56 +0000

    Adding LIKE BINARY to force case-sensitive comparison

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
                          host="localhost")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name\
+            LIKE BINARY 'N%' ORDER BY id ASC")
 
     for row in cur.fetchall():
         print(row)
