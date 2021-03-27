3dc22d91b0a4a43bd408d6c664e1954dffbcc81d
andresgfranco <andresgfranco0@gmail.com>
2021-03-27 16:25:08 +0000

    Correction on SQL query, pep8 style and function documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,14 +6,15 @@ import sys
 
 
 def main():
+    """Function that lists all states with a name starting with N"""
     db = MySQLdb.connect(user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            host="localhost")
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host="localhost")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     for row in cur.fetchall():
         print(row)
