54bf2b50b066523b6d994bc69dba6b2f8f8211fd
andresgfranco <andresgfranco0@gmail.com>
2021-03-28 02:01:04 +0000

    Adding Louisiana as a state using session.add and session.commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script that prints the first State object from db
+"""
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """Function that lists all State objects from the db"""
+    credentials = {
+        "host": "localhost",
+        "port": "3306",
+        "user": argv[1],
+        "pass": argv[2],
+        "db": argv[3]}
+
+    str_format = 'mysql+mysqldb://{user}:{pass}@{host}:{port}/{db}'
+    engine = create_engine(str_format.format(
+        **credentials), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name="Lousiana")
+
+    session.add(louisiana)
+    session.commit()
+
+    print(louisiana.id)
+
+    session.close()
