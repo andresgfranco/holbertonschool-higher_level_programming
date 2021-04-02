188a645b67740dd3f3edffeaecee55f81a922e2d
andresgfranco <andresgfranco0@gmail.com>
2021-04-02 23:01:04 +0000

    Adding advanced task 17

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" script that lists all City objects from the database hbtn_0e_101_usa"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+
+
+def main():
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(mysql_username,
+                                                           mysql_password,
+                                                           database_name))
+
+    engine = create_engine(db, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    queryCities = session.query(State).order_by(State.id).all()
+
+    for state in queryCities:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
