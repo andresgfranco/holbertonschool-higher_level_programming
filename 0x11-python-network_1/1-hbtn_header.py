593eca0c9bde2eee0a822d99327397c7a1d0685e
andresgfranco <andresgfranco0@gmail.com>
2021-04-12 01:36:35 +0000

    Adding script number 1 to send a request to the URL given

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of
+the response"""
+from urllib import request
+import sys
+
+
+if __name__ == "__main__":
+        with request.urlopen(sys.argv[1]) as response:
+                print(response.getheader("X-Request-Id"))
