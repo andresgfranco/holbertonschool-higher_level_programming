654e13ec0650db2d531047974d2c5284682346b1
andresgfranco <andresgfranco0@gmail.com>
2021-04-12 04:04:09 +0000

    Correction on pep8 style

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == "__main__":
     req = requests.get("https://api.github.com/user",
-                        auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
-    
+                       auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+
     print(req.json().get("id"))
